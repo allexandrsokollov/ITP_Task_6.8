641c1ac24af07c147e37b844e3eb3f9c44ddb19e
allexandersokollov <allexandersokollov@gmail.com>
2021-10-15 12:39:32 +0300

    task is done

--- src/vsu/csf/sokolov_aa/Main.java
+++ src/vsu/csf/sokolov_aa/Main.java
@@ -8,9 +8,13 @@ public class Main {
     public static void main(String[] args) {
         Locale.setDefault(Locale.ROOT);
 	    int n = getIntFromConsole("enter N");
-        double x = getDoubleFromConsole("Enter X");
+        double x = getDoubleFromConsole("\nEnter X");
+        double eps = getDoubleFromConsole("\nEnter eps");
 
-        System.out.println(findSumOfElem(n, x));
+        System.out.println("\nSum of elements is " + findSumOfElem(n, x));
+        System.out.println("Sum of elements that bigger then eps is " + findSumOfElemThatBiggerThanEps(eps, x));
+        System.out.println("Sum of elements that bigger then eps / 10 is " + findSumOfElemThatBiggerThanEps(eps / 10, x));
+        System.out.println("Value via Math library is " + Math.sqrt(1 + x));
     }
 
     public static double findSumOfElem(int n, double x) {
@@ -30,6 +34,23 @@ public class Main {
         return sum;
     }
 
+    public static double findSumOfElemThatBiggerThanEps(double eps, double x) {
+        double sum = 1;
+        double prev = 1;
+
+        for(int i = 1; prev > eps; i++) {
+
+            if(isOdd(i)) {
+                prev *= (i * x) / (2 * i);
+            } else {
+                prev *=  (-x) / (2 * i);
+            }
+            sum += prev;
+        }
+
+        return sum;
+    }
+
 
     public static boolean isOdd(int number) {
         return number % 2 != 0;
@@ -37,14 +58,14 @@ public class Main {
 
     public static int getIntFromConsole(String message) {
         Scanner in = new Scanner(System.in);
-        System.out.print("\n" + message + ": ");
+        System.out.print(message + ": ");
         return in.nextInt();
     }
 
 
     public static double getDoubleFromConsole(String message) {
         Scanner in = new Scanner(System.in);
-        System.out.print("\n" + message + ": ");
+        System.out.print(message + ": ");
         return in.nextDouble();
     }
 
