375140367ccc7aed47142da47deced83bd248635
VladaWeisberg <e_nagibina01@mail.ru>
2021-10-21 11:00:14 +0300

    add solomatin version

--- src/vsu/csf/sokolov_aa/Main.java
+++ src/vsu/csf/sokolov_aa/Main.java
@@ -4,22 +4,6 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
-
-    public static void main(String[] args) {
-        Locale.setDefault(Locale.ROOT);
-
-	    int n = getIntFromConsole("enter N");
-        double x = getDoubleFromConsole("\nEnter X");
-        double eps = getDoubleFromConsole("\nEnter eps");
-
-        double[] result = findSumOfElem(n, x, eps);
-
-        System.out.println("\nSum of elements is " + result[2]);
-        System.out.println("Sum of elements that bigger then eps is " + result[0]);
-        System.out.println("Sum of elements that bigger then eps / 10 is " + result[1]);
-        System.out.println("Value via Math library is " + Math.sqrt(1 + x));
-    }
-
     public static double[] findSumOfElem(int n, double x, double eps) {
         final double[]  sum = new double[3];
         double prev = 1;
@@ -46,6 +30,55 @@ public class Main {
         }
 
         return sum;
+    }
+
+    public static double[] solomatinVersion(int n, double x, double eps) {
+        final double[]  sum = new double[3];
+        double prev = 1;
+
+        for (int i = 1; (Math.abs(prev) >= eps / 10) || i <= n; i++) {
+
+            if (Math.abs(prev) >= eps) {
+                sum[0] += prev;
+            }
+
+            if (Math.abs(prev) >= eps / 10) {
+                sum[1] += prev;
+            }
+
+            if (i <= n) {
+                sum[2] += prev;
+            }
+
+
+            prev = -prev * ((2 * i -3 ) * x / (2 * i - 1));
+
+        }
+
+        return sum;
+    }
+
+
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.ROOT);
+
+	    int n = getIntFromConsole("enter N");
+        double x = getDoubleFromConsole("\nEnter X");
+        double eps = getDoubleFromConsole("\nEnter eps");
+
+        double[] result = findSumOfElem(n, x, eps);
+
+        double[] solomatinResult = solomatinVersion(n, x, eps);
+
+        System.out.printf("\nSum of elements is %.010f", result[2]);
+        System.out.printf("\nSum of elements that bigger then eps is %.010f", result[0]);
+        System.out.printf("\nSum of elements that bigger then eps / 10 is %.010f", result[1]);
+        System.out.printf("\nValue via Math library is %.010f", Math.sqrt(1 + x));
+
+        System.out.printf("\n\n\nSum of elements is %.010f", solomatinResult[2]);
+        System.out.printf("\nSum of elements that bigger then eps is %.010f", solomatinResult[0]);
+        System.out.printf("\nSum of elements that bigger then eps / 10 is %.010f", solomatinResult[1]);
+        System.out.printf("\nValue via Math library is %.010f", Math.sqrt(1 + x));
     }
 
     public static boolean isOdd(int number) {
